6a13dc597d6229b58ae1a324600abf6f59968292
Shivang Rathore <shivang.rathore@flightkite.com>
2025-11-05 16:02:56 +0530

    fix: use production forms

--- config.ts
+++ config.ts
@@ -1,22 +1,23 @@
 export const contactFormUrl =
-  "https://docs.google.com/forms/d/e/1FAIpQLScutw9PPcqUTs_0KIaZu_8x73J09AD_cz_ZowdzSHGh9qlU3w/formResponse";
+  "https://docs.google.com/forms/d/e/1FAIpQLSdm8pXNpPE80iWP9P9YAnXU3G9Wpaf0ViAPgDUQq33IhVagvw/formResponse";
+
 export const contactFormFields = {
-  firstName: "entry.339045996",
-  lastName: "entry.1613011660",
-  email: "entry.1324479998",
-  phone: "entry.1522846725",
-  enquiry: "entry.51310740",
+  firstName: "entry.1027562968",
+  lastName: "entry.199425292",
+  email: "entry.1535706841",
+  phone: "entry.500733271",
+  enquiry: "entry.1046861596",
 };
 
 export const valuationFormUrl =
-  "https://docs.google.com/forms/d/e/1FAIpQLSc8K_wAumrb0LCcIvwNiK-CblRSrFM4wwv7JczavDJao50w0A/formResponse";
+  "https://docs.google.com/forms/d/e/1FAIpQLSftgLzO14_6ANuWWQ2fDFxc0rYVSzdcacd5H9m0EhgtfnQK6Q/formResponse";
 
 export const valuationFormFields = {
-  firstName: "entry.681707482",
-  lastName: "entry.688835227",
-  email: "entry.1950336826",
-  address: "entry.189085420",
-  phone: "entry.954835114",
-  postalCode: "entry.1987663072",
-  valuationType: "entry.1165352326",
+  valuationType: "entry.901055619",
+  firstName: "entry.278958287",
+  lastName: "entry.1076645919",
+  email: "entry.1318453569",
+  address: "entry.1847474231",
+  phone: "entry.23215775",
+  postalCode: "entry.1294446381",
 };
